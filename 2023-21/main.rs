3bf27aa9abe89d061974b47238c16f9de82e884b
Tomáš Zahradníček <zahradnicek.tom@seznam.cz>
2023-12-21 22:36:21 +0100

    2023-21

--- 2023-21/main.rs
+++ 2023-21/main.rs
@@ -1,7 +1,7 @@
 use std::collections::{HashMap, HashSet};
-use std::hash::{Hash, Hasher};
-use std::{io, mem};
-use std::ops::Index;
+use std::hash::Hash;
+use std::io;
+use std::ops::{Add, Index};
 use anyhow::{Result, Ok, bail, Context};
 
 struct Puzzle {
@@ -27,6 +27,21 @@ impl Map {
     fn is_valid(&self, coord: &Coord) -> bool {
         coord.x >= 0 && coord.y >= 0 && coord.x < self.columns as i64 && coord.y < self.rows as i64
     }
+
+    // returns a coord that is valid in current Map. If input is not valid,
+    // it will be wrapped into valid one, with result Offset representing number of "submaps" moved
+    // in specified directions.
+    fn mod_coord(&self, coord: &Coord) -> (Coord, Offset) {
+        let (x_div, x_rem) = modulo(coord.x, self.columns as i64);
+        let (y_div, y_rem) = modulo(coord.y, self.rows as i64);
+        (Coord {
+            x: x_rem,
+            y: y_rem,
+        }, Offset {
+            x: x_div,
+            y: y_div,
+        })
+    }
 }
 
 #[derive(Eq, PartialEq, Debug, Copy, Clone, Hash)]
@@ -50,57 +65,31 @@ enum Point {
     Rock,
 }
 
-#[derive(Copy, Clone)]
+#[derive(Copy, Clone, Eq, PartialEq, Hash, Debug)]
 struct Offset {
     x: i64,
     y: i64,
 }
 
-#[derive(Clone, Eq, PartialEq, Debug)]
-struct PositionsId {
-    positions: HashSet<Coord>,
-}
+impl Add for Offset {
+    type Output = Offset;
 
-impl Hash for PositionsId {
-    fn hash<H: Hasher>(&self, state: &mut H) {
-        // todo: maybe not deterministic hash? (at least in the same program run - don't care about different runs)
-        self.positions.iter().collect::<Vec<_>>().hash(state)
-    }
-}
-
-struct PositionTracker {
-    visited_submap_states: HashMap<PositionsId, PositionsId>,
-    current_submap_states: HashMap<PositionsId, i64>,
-}
-
-impl PositionTracker {
-    fn move_from_positions(&mut self, map: &Map) {
-        let mut prev_submap_states = HashMap::<PositionsId, i64>::new();
-        mem::swap(&mut self.current_submap_states, &mut prev_submap_states);
-        for (submap_state, submap_state_count) in prev_submap_states {
-            let visited = self.visited_submap_states.get(&submap_state);
-            match visited {
-                None => {
-                    let next_state = move_from_positions(&map, &submap_state.positions);
-                    let current_positions_id = PositionsId { positions: submap_state.positions };
-                    let next_positions_id = PositionsId { positions: next_state };
-                    *self.current_submap_states.entry(next_positions_id.clone()).or_default() += 1;
-                    self.visited_submap_states.insert(current_positions_id, next_positions_id);
-                }
-                Some(next) => {
-                    *self.current_submap_states.entry(next.clone()).or_default() += submap_state_count;
-                }
-            }
+    fn add(self, rhs: Self) -> Self::Output {
+        Offset {
+            x: self.x + rhs.x,
+            y: self.y + rhs.y,
         }
     }
+}
 
-    fn count(&self) -> i64 {
-        let mut sum = 0;
-        for (submap_state, submap_state_count) in &self.current_submap_states {
-            sum += submap_state.positions.len() as i64 * submap_state_count;
-        }
-        sum
+fn modulo(num: i64, divisor: i64) -> (i64, i64) {
+    let mut div = num / divisor;
+    let mut rem = num % divisor;
+    if rem < 0 {
+        div -= 1;
+        rem += divisor;
     }
+    (div, rem)
 }
 
 static UP: Offset = Offset { x: 0, y: -1 };
@@ -112,39 +101,109 @@ fn main() -> Result<()> {
     let puzzle = parse()?;
 
     // part 1
+    let reachable_exactly_in: i64 = 64;
     let mut positions: HashSet<Coord> = [puzzle.start].into();
-    for _ in 0..64 {
+    for _ in 0..reachable_exactly_in {
         positions = move_from_positions(&puzzle.map, &positions);
     }
 
     println!("{}", positions.len());
 
     // part 2
-    let mut positions_tracker = PositionTracker {
-        visited_submap_states: Default::default(),
-        current_submap_states: [(
-            PositionsId { positions: HashSet::<Coord>::from([puzzle.start]) },
-            1,
-        )].into(),
-    };
+    let reachable_exactly_in: i64 = 26501365;
+    assert_eq!(puzzle.map.rows, puzzle.map.columns);
+    let across = puzzle.map.rows as i64;
+    assert_eq!(across % 2, 1);
+    let mid = across / 2;
+    assert_eq!(puzzle.start, Coord { x: mid, y: mid });
 
-    for _ in 0..64 {
-        positions_tracker.move_from_positions(&puzzle.map)
+    let mut extended_positions_reachability: HashMap<Offset, HashMap<Coord, i64>> = Default::default();
+    for offset in [
+        Offset { x: 0, y: 0 },
+        UP,
+        UP + RIGHT,
+        RIGHT,
+        RIGHT + DOWN,
+        DOWN,
+        DOWN + LEFT,
+        LEFT,
+        LEFT + UP,
+    ] {
+        extended_positions_reachability.insert(offset, Default::default());
+        if offset.x == 0 && offset.y == 0 {
+            extended_positions_reachability.get_mut(&offset).unwrap().insert(puzzle.start, 0);
+        }
     }
 
-    println!("{}", positions_tracker.count());
+    for i in 0.. {
+        let prev_extended_positions_reachability: HashMap<Offset, HashMap<Coord, i64>> = extended_positions_reachability.clone();
+        for (offset, offset_reachability) in prev_extended_positions_reachability.clone() {
+            let mut from_coords: HashSet<Coord> = Default::default();
+            for (reachable, reachable_in) in offset_reachability {
+                if reachable_in != i {
+                    continue;
+                }
+                from_coords.insert(reachable);
+            }
+            let unbounded_to_coords = move_from_positions_unbounded(&puzzle.map, &from_coords);
+            for reached in unbounded_to_coords {
+                let (wrapped_coord, wrapped_offset) = puzzle.map.mod_coord(&reached);
+                let sum_offset = offset + wrapped_offset;
+                if sum_offset.x.abs() >= 2 || sum_offset.y.abs() >= 2 {
+                    continue;
+                }
+                extended_positions_reachability.get_mut(&sum_offset).unwrap().entry(wrapped_coord).or_insert(i + 1);
+            }
+        }
+        if prev_extended_positions_reachability == extended_positions_reachability {
+            break;
+        }
+    }
+
+    let mut sum = 0;
+    // center submap
+    for &reachable_in in extended_positions_reachability.get(&Offset { x: 0, y: 0 }).unwrap().values() {
+        if reachable_in <= reachable_exactly_in && (reachable_exactly_in - reachable_in) % 2 == 0 {
+            sum += 1
+        }
+    }
+    // in cardinal directions
+    for offset in [UP, RIGHT, DOWN, LEFT] {
+        for &reachable_in in extended_positions_reachability.get(&offset).unwrap().values() {
+            if reachable_in > reachable_exactly_in {
+                continue;
+            }
+            let diff = reachable_exactly_in - reachable_in;
+
+            sum += cardinal_count(diff, across);
+        }
+    }
+    // in diagonal directions
+    for offset in [UP + RIGHT, RIGHT + DOWN, DOWN + LEFT, LEFT + UP] {
+        for &reachable_in in extended_positions_reachability.get(&offset).unwrap().values() {
+            if reachable_in > reachable_exactly_in {
+                continue;
+            }
+            let diff = reachable_exactly_in - reachable_in;
+            sum += diagonal_count(diff, across);
+        }
+    }
+
+    println!("{}", sum);
 
     Ok(())
 }
 
 fn move_from_positions(map: &Map, positions: &HashSet<Coord>) -> HashSet<Coord> {
+    move_from_positions_unbounded(map, positions).into_iter().filter(|x| map.is_valid(x)).collect()
+}
+
+fn move_from_positions_unbounded(map: &Map, positions: &HashSet<Coord>) -> HashSet<Coord> {
     let mut result = HashSet::<Coord>::default();
     for from_coord in positions {
         for direction in [UP, LEFT, DOWN, RIGHT] {
             let to_coord = from_coord.offset(direction);
-            // todo: somehow detects "leaks" to other submaps. But don't know how to
-            // keep track of "leaks" from multiple neighbors at the same time
-            if map.is_valid(&to_coord) && matches!(map.index_coord(&to_coord), Point::Garden) {
+            if !map.is_valid(&to_coord) || matches!(map.index_coord(&to_coord), Point::Garden) {
                 result.insert(to_coord);
             }
         }
@@ -183,4 +242,109 @@ fn parse() -> Result<Puzzle> {
         },
         start: start.context("no start found")?,
     })
+}
+
+fn cardinal_count(diff: i64, across: i64) -> i64 {
+    // max number of submaps this point is reachable in that direction
+    // S01234
+    let reachable_in_submap_count = diff / across;
+    let parity = (reachable_in_submap_count + diff) % 2;
+    ((reachable_in_submap_count + parity) / 2) + ((parity + 1) % 2)
+}
+
+fn diagonal_count(diff: i64, across: i64) -> i64 {
+    // number this point is reachable in that diagonal direction
+    // .43456
+    // .34567
+    // .23456
+    // .12345
+    // .01234
+    // S....
+    let reachable_in_diagonal = diff / across;
+    let parity = (reachable_in_diagonal + diff % across) % 2;
+
+    // let reachable_in_diagonal be n;
+    //
+    // for parity 0:
+    // result = 1 + 3 + 5 + 7 ... (n/2+1 items)
+    // let x = n/2+1
+    // result = 1 + 3 + 5 + 7 ... (x items)
+    // result = -x + (2 + 4 + 6 + 8 ... (x items))
+    // result = -x + 2 * (x * (x+1) / 2)
+    // result = -x + (x * (x + 1))
+    // result = -x + (x * (x + 1))
+    // result = x * x
+    //
+    // for parity 1:
+    // result = 2 + 4 + 6 + 8 ... ((n+1)/2 items)
+    // result = 2 * (1 + 2 + 3 + 4 ... ((n+1)/2 items)))
+    // let x = (n+1)/2
+    // result = 2 * (x * (x+1) / 2)
+    // result = x * (x+1)
+    //
+    match parity {
+        0 => {
+            let x = reachable_in_diagonal / 2 + 1;
+            x * x
+        }
+        1 => {
+            let x = (reachable_in_diagonal+1) / 2;
+            x * (x + 1)
+        }
+        _ => unreachable!()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use rstest::rstest;
+    use crate::{cardinal_count, diagonal_count};
+
+    #[rstest]
+    #[case(0, 1, 1)]
+    #[case(1, 1, 1)]
+    #[case(2, 1, 2)]
+    #[case(3, 1, 2)]
+    #[case(4, 1, 3)]
+    #[case(5, 1, 3)]
+    #[case(0, 3, 1)]
+    #[case(1, 3, 0)]
+    #[case(2, 3, 1)]
+    #[case(3, 3, 1)]
+    #[case(4, 3, 1)]
+    #[case(5, 3, 1)]
+    #[case(6, 3, 2)]
+    #[case(7, 3, 1)]
+    #[case(0, 131, 1)]
+    #[case(131, 131, 1)]
+    #[case(132, 131, 1)]
+    #[case(262, 131, 2)]
+    #[case(263, 131, 1)]
+    fn test_cardinal_count(#[case] diff: i64, #[case] across: i64, #[case] expected: i64) {
+        assert_eq!(cardinal_count(diff, across), expected)
+    }
+
+    #[rstest]
+    #[case(0, 1, 1)]
+    #[case(1, 1, 2)]
+    #[case(2, 1, 4)]
+    #[case(3, 1, 6)]
+    #[case(4, 1, 9)]
+    #[case(5, 1, 12)]
+    #[case(0, 3, 1)]
+    #[case(1, 3, 0)]
+    #[case(2, 3, 1)]
+    #[case(3, 3, 2)]
+    #[case(4, 3, 1)]
+    #[case(5, 3, 2)]
+    #[case(6, 3, 4)]
+    #[case(7, 3, 2)]
+    #[case(0, 131, 1)]
+    #[case(131, 131, 2)]
+    #[case(132, 131, 1)]
+    #[case(262, 131, 4)]
+    #[case(263, 131, 2)]
+    fn test_diagonal_count(#[case] diff: i64, #[case] across: i64, #[case] expected: i64) {
+        assert_eq!(diagonal_count(diff, across), expected)
+    }
 }
